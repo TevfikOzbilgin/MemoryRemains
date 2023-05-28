ac888dbb54c44bc0cb888b40176e5bf6d3fcfb66
ttt <tevfik_ozbilgin@hotmail.com>
2023-05-28 18:26:27 +0300

    Changed the HTML for the site.

--- MemoryRemains/settings.py
+++ MemoryRemains/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
